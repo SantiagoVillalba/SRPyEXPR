13bfa96132d5a8daff54a6b2766fa2974f03c5da
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-09-01 19:27:44 -0300

    EXPERT

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -4,6 +4,10 @@ namespace Expert_SRP
 {
     public class Alfajor 
     {
+        // Aplica el principio de EXPERT ya que la clase alfajor 
+        // solo tiene la responsabilidad de conocer los precios de la
+        // masa y el dulce. Y es lo unico que hace. 
+        
         public Alfajor(double precioMasa, double precioDulce)
         {
             this.PrecioDulce = precioDulce;
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,6 +4,11 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
+        // El puede comprar tiene sentido en kiosko ya que este es el que vende el alfajor,
+        // entonces por el principio EXPERT, este deberia de saber los precios de los
+        // alfajores y la plata que tiene la persona para pagar los alfajores.\
+        // por esto mismo tambien tiene sentido que ConvertirAPesos se encargue de la
+        // conversion de la moneda.
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
