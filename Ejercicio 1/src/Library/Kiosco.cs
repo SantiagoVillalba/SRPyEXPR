53dfcd68924158daf81b61d0d15cfa1b9b372e50
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-09-01 19:52:55 -0300

    EXPR

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -5,8 +5,9 @@ namespace Expert_SRP
     public class Kiosco 
     {
         // El puede comprar tiene sentido en kiosko ya que este es el que vende el alfajor,
-        // entonces por el principio EXPERT, este deberia de saber los precios de los
-        // alfajores y la plata que tiene la persona para pagar los alfajores.
+        // entonces por el principio EXPERT,pero este no deberia de saber los precios de los
+        // alfajores, pero si la plata que tiene la persona para pagar los alfajores, seria mejor
+        // si calcular el precio del alfajor se lo pusieramos al alfajor mismo.
         // Por esto mismo tambien tiene sentido que ConvertirAPesos se encargue de la
         // conversion de la moneda ya que es la clase que sabe la moneda que esta el dinero.
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
