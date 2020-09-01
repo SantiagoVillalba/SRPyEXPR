f29d66978347312f9c1710e68d7837f389eab317
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-09-01 19:35:31 -0300

    SRP

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -2,6 +2,8 @@ using System;
 
 namespace Expert_SRP
 {
+    // Para mi se aplica el SRP, ya que este solo tiene la responsabilidad de 
+    //de conocerse a si mismo, al alfajor en si.
     public class Alfajor 
     {
         public Alfajor(double precioMasa, double precioDulce)
--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -4,6 +4,13 @@ namespace Expert_SRP
 {
     public class Kiosco 
     {
+        // El principio de SRP en esta clase esta mal, ya que a pesar de ser el 
+        // experto en datos para tener estas responsabilidades, en esta clase hay 2 razones
+        // de cambio, por ejemplo el poder comprar, tiene sentido en el kiosko, y que sea su
+        // responsabilidad, pero ConvertirAPesos quedaria mejor en una clase de por ejemplo
+        // moneda, y asi tambien podrias saber si los alfajores estan en otra moneda, sin 
+        // tener que modificar ninguna de las clases. 
+
         public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
         {
             Double pesos = ConvertirAPesos(dinero, moneda);
