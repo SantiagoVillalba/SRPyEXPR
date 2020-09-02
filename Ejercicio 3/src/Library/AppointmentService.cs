c9ff98755ec71de006887b122fd80b6eef6c7c7d
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-09-01 21:00:19 -0300

    Update AppointmentService.cs

--- Ejercicio 3/src/Library/AppointmentService.cs
+++ Ejercicio 3/src/Library/AppointmentService.cs
@@ -7,7 +7,7 @@ namespace Library
     {
         /* Segun el principio SRP este codigo esta mal, ya que una consulta solo deberia de saber la fecha y el lugar de la consulta
         , pero como vemos en este codigo sabe tanto datos del paciente, como del doctor, cosas que deberian de estar en otras clases, conociendo
-        sus propios datos. Esta clase presentada en el codigo deberia de ser cambiada varias veces, en el caso de que queramos cambiar datos tanto
+        sus propios datos. Esta clase presentada en el codigo deberia de ser cambiada varias veces, en el caso de que queramos cambiar o agregar datos tanto
         del paciente como del doctor, por lo tanto no cumple con el principio SRP. Sobre el patron Expert este codigo tambien esta mal, ya que los datos tanto 
         del paciente como del doctor, la clase AppointmentService no es la experta en estos datos, por eso no lo cumple el patron. Para arreglar este codigo
         deberiamos agregar una clase Doctor y una clase Persona, para asi que cada una se encargue de conocer los datos de ellos mismos.  */
