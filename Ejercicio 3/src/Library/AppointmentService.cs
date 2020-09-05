155f4d0771df00e80e179294b3fa04d5afceb48c
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-09-05 19:54:32 -0300

    final

--- Ejercicio 3/src/Library/AppointmentService.cs
+++ Ejercicio 3/src/Library/AppointmentService.cs
@@ -5,6 +5,21 @@ namespace Library
 {
     public class AppointmentService
     {
+        /* En esta clase me encargo de conocer las cosas necesarias para poder hacer una 
+        consulta , como el Codigo de la consulta , la fecha, el lugar de la consulta
+        y por ultimo el Doctor y el Paciente. Estos son objetos de otras clases Doctor y 
+        Paciente respectivamente, esto hace que se pueda cumplir el patron SRP ya que 
+        en esta clase solo tenemos lo respectivo a la Consulta o Appointment, abstrayendonos
+        de los demas objetos y sus responsabilidades, asi cumpliendo con la encapsulacion, y 
+        en el caso de que se quieran cambiar las otras clases esta no se va a ver afectada,
+        ya que tiene solo una razon de cambio.
+        Tambien cumple con el patron Expert ya que esta clase solo se encarga de conocer
+        lo necesario de la Consulta y es experta en ello. */
+
+        // Esta variable estatica se usa para que cada vez que se instancie un nuevo Appointment
+        // esta funcione como contador, y el codigo unico va a tomar el valor de esta variable,
+        // de esta manera todas los objetos instanciados de esta clase, tienen un identificador 
+        // unico.
         private static int codigo=0;
         public int CodigoUnico{get;}
         public DateTime Date{get;set;}
@@ -28,6 +43,11 @@ namespace Library
 
 
         }
+
+        // En vez de usar un metodo para anotar un appointment , que en realidad solo devolvia
+        // un string sin hacer ningun appointment , uso este metodo en el constructor, asi poder 
+        // crear realmente un objeto por cada consulta, tambien sin perder ese string, que nos 
+        // decia cual era el fallo que habia en la creacion.
         public static bool CreateAppointment(DateTime date, string appoinmentPlace,Paciente paciente, Doctor doctor)
         {
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
--- Ejercicio 3/src/Library/Doctor.cs
+++ Ejercicio 3/src/Library/Doctor.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 namespace Library
 {
+    /* Una clase que se encarga de tener la responsabilidad de conocer al Doctor y 
+    verificar sus datos. Con un metodo de clase para verificar otras instancias de la 
+    clase Doctor. */
     public class Doctor
     {
         public string Name {get;set;}
